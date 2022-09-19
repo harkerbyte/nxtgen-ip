9d3013c743ad564bbe5d4498418b095975298b0c
Cybernetics <79071277+shade234sherif@users.noreply.github.com>
2022-09-19 09:51:04 +0100

    Update ip.py

--- ip.py
+++ ip.py
@@ -31,41 +31,41 @@ while answer:
        
 
 
-        def location_data():
+        def locate_data():
             lookup = 'https://ipapi.co'
             response = requests.get(f'{lookup}/{the_ip}/json/').json()
             location_data={
                 "ip": the_ip,
-                "org": response.get('org'),
-                "hostname": response.get('hostname'),
-                "version": response.get('version'),
-                "city": response.get('city'),
-                "country": response.get('country'),
-                "country_code": response.get('country_code'),
-                "country_name": response.get('country_name'),
-                "country_code_iso3": response.get('country_code_iso3'),
-                "country_capital": response.get('country_capital'),
-                "country_tld": response.get('country_tld'),
-                "country_area": response.get('country_area'),
-                "country_population": response.get('country_population'),
-                "region": response.get('region'),
-                "region_code": response.get('region_code'),
-                "continent_code": response.get('continent_code'),
-                "in_europe": response.get('in_eu'),
-                "postal": response.get('postal'),
-                "latitude": response.get('latitude'),
-                "longitude": response.get('longitude'),
-                "timezone": response.get('timezone'),
-                "utc_offset": response.get('utc_offset'),
-                "country_calling_code": response.get('country_calling_code'),
-                "currency": response.get('currency'),
-                "currency_name": response.get('currency_name'),
-                "languages": response.get('languages'),
+                "org": response('org'),
+                "hostname": response('hostname'),
+                "version": response('version'),
+                "city": response('city'),
+                "country": response('country'),
+                "country_code": response('country_code'),
+                "country_name": response('country_name'),
+                "country_code_iso3": response('country_code_iso3'),
+                "country_capital": response('country_capital'),
+                "country_tld": response('country_tld'),
+                "country_area": response('country_area'),
+                "country_population": response('country_population'),
+                "region": response('region'),
+                "region_code": response('region_code'),
+                "continent_code": response('continent_code'),
+                "in_europe": response('in_eu'),
+                "postal": response('postal'),
+                "latitude": response('latitude'),
+                "longitude": response('longitude'),
+                "timezone": response('timezone'),
+                "utc_offset": response('utc_offset'),
+                "country_calling_code": response('country_calling_code'),
+                "currency": response('currency'),
+                "currency_name": response('currency_name'),
+                "languages": response('languages'),
             }
             return location_data
-            print(location_data())
+            print(locate_data())
             file = open('results.txt','x')
-            file.write(location_data())
+            file.write(locate_data())
             file.close()
 
 
