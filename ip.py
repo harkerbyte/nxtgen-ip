faaaf3acd561209e2e0cf104d83a7514f6e56c8d
Shade <79071277+harkerbyte@users.noreply.github.com>
2025-01-10 19:29:16 +0100

    update ip.py

--- ip.py
+++ ip.py
@@ -32,7 +32,7 @@ while answer:
 
   elif answer == "2":
       the_ip = input("INPUT THE IP ADDRESS HERE:")
-      if isinstance(the_ip, int) or isinstance(the_ip, float):
+      if isinstance(the_ip, float):
         file_type = 'json'
         lookup = 'https://ipapi.co'
         
