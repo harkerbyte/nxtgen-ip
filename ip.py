96328c2c1f18e9d0c114274fe21d6ce7fecf05f3
Shade <79071277+harkerbyte@users.noreply.github.com>
2025-01-10 15:31:58 +0100

    update ip.py

--- ip.py
+++ ip.py
@@ -73,7 +73,7 @@ while answer:
       file.write(json.dumps(locate_data(), indent=4))
       file.close()
       
-      for key in locate_data().value:
+      for key in locate_data().keys:
         if key["latitude"] or key["longitude"] != "null":
         
           gen = True
