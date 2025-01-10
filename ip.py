8fcb4ef06bc20c9d830423055d7452c922831dd8
Shade <79071277+harkerbyte@users.noreply.github.com>
2025-01-10 21:27:56 +0100

    update ip.py

--- ip.py
+++ ip.py
@@ -32,7 +32,7 @@ while answer:
 
   elif answer == "2":
       the_ip = input("INPUT THE IP ADDRESS HERE:")
-      if isinstance(int(the_ip), (int, float)):
+      try float(the_ip):
         file_type = 'json'
         lookup = 'https://ipapi.co'
         
@@ -95,7 +95,7 @@ while answer:
              elif gen!='':
                print("Command not recognised")
         print('\033[2;31mNo data was captured\033[2;0m')     
-      else:
+      except ValueError:
         print('\033[2;31mThat wasn\'t an ip address\033[2;0m')
       
   elif answer == '3':
